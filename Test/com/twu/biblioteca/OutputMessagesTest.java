c66f0319c44df66564860d5551a1d5a8f002e2d1
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-06 10:29:20 +0530

    Passed Spec for welcome message

---
+++ Test/com/twu/biblioteca/OutputMessagesTest.java
@@ -0,0 +1,34 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class OutputMessagesTest {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void shouldBeWelcomeMessage() {
+        OutputMessages system = new OutputMessages();
+
+        system.displayWelcomeMessage();
+
+        assertEquals("Welcome To Biblioteca!!!\n", outContent.toString());
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+
+    }
+
+}
---
+++ src/com/twu/biblioteca/OutputMessages.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca;
+
+//Print Ouput Message onto the Screen
+public class OutputMessages {
+
+    public void displayWelcomeMessage()
+    {
+        System.out.println("Welcome To Biblioteca!!!");
+    }
+}
