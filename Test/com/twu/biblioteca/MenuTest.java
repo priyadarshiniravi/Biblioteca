1fe7f076e66563953054621d01a28dce38b6026c
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-09 22:14:37 +0530

    Passed Spec to Should listen to invalid Option

--- Test/com/twu/biblioteca/MenuTest.java
+++ Test/com/twu/biblioteca/MenuTest.java
@@ -1,8 +1,12 @@
 package com.twu.biblioteca;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 
 import static org.mockito.Mockito.mock;
@@ -10,9 +14,16 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class MenuTest {
+    final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
 
     @Test
-    public void shouldListenToOption() throws IOException {
+    public void shouldListenToOptionGetBooks() throws IOException {
         MenuToConsole menuToConsoleStub = mock(MenuToConsole.class);
         when(menuToConsoleStub.getMenuOption())
                 .thenReturn(1);
@@ -21,6 +32,21 @@ public class MenuTest {
 
         menu.listenToOption(menuToConsoleStub);
         verify(menuToConsoleStub).getMenuOption();
+
+
+    }
+
+    @Test
+    public void shouldListenToInvalidOption() throws IOException {
+        MenuToConsole menuToConsoleStub = mock(MenuToConsole.class);
+        when(menuToConsoleStub.getMenuOption())
+                .thenReturn(2);
+        BooksToConsole bookToConsoleStub = mock(BooksToConsole.class);
+        Menu menu = new Menu(bookToConsoleStub);
+
+        menu.listenToOption(menuToConsoleStub);
+
+        Assert.assertEquals("Invalid Option\n", outContent.toString());
 
 
     }
--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Menu {
-    HashMap<Integer, MenuListener> menuItems = new HashMap<Integer, MenuListener>();
+    HashMap<Integer, MenuListener> menuItems = new HashMap();
 
     public Menu(BooksToConsole booksToConsole) {
         menuItems.put(1, new ListBooks(booksToConsole));
@@ -14,6 +14,9 @@ public class Menu {
 
     public void listenToOption(MenuToConsole menuDisplay) throws IOException {
         int optionAsInteger = menuDisplay.getMenuOption();
+        if (menuItems.get(optionAsInteger)==null)
+            System.out.println("Invalid Option");
+        else
         menuItems.get(optionAsInteger).displayStrategy();
     }
 
