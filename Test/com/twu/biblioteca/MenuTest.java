f4296d0d28da821d02495c7db914fc342cb1ad0e
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-12 01:03:17 +0530

    Passed Spec Should take Input

--- Test/com/twu/biblioteca/MenuTest.java
+++ Test/com/twu/biblioteca/MenuTest.java
@@ -3,7 +3,27 @@ package com.twu.biblioteca;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class MenuTest {
-    
+    @Test
+    public void ShouldTakeInput()
+    {
+        ConsoleInputOutput consoleInputOutput=mock(ConsoleInputOutput.class);
+        Parser parser=mock(Parser.class);
+        Menu menu=new Menu(consoleInputOutput,parser);
+        Options options=mock(Options.class);
+        when(consoleInputOutput.getInput())
+                .thenReturn(1);
+        when(parser.parse(1))
+                .thenReturn(options);
+
+        menu.chooseOption();
+
+        verify(consoleInputOutput).getInput();
+    }
+
 
 }
