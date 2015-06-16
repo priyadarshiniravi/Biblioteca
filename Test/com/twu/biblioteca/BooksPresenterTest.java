934cbd20c6b683ea62caffad43d4c3eaf5e5db47
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-16 14:08:14 +0530

    Added funtionality AddBook to Book Presentor

--- Test/com/twu/biblioteca/BooksPresenterTest.java
+++ Test/com/twu/biblioteca/BooksPresenterTest.java
@@ -16,4 +16,16 @@ public class BooksPresenterTest {
         assertEquals("MyString", actualString);
     }
 
+    @Test
+    public void shouldAddBookToTheFormattedMovieList() {
+        String FormattedMovies = new String();
+        BooksPresenter booksPresenter = new BooksPresenter(FormattedMovies);
+
+        booksPresenter.addBook("C++", "ABC", 1998);
+        booksPresenter.addBook("UP", "xyz", 1987);
+        String actualFormattedMovies = booksPresenter.toString();
+
+        assertEquals("UP                            |xyz                           |1987", actualFormattedMovies);
+    }
+
 }
--- src/com/twu/biblioteca/BooksPresenter.java
+++ src/com/twu/biblioteca/BooksPresenter.java
@@ -2,6 +2,8 @@ package com.twu.biblioteca;
 
 public class BooksPresenter {
     private String formattedBooks;
+    private final int maximunNameLength = 30;
+    private final int maximumAuthorLength = 30;
 
     public BooksPresenter(String formattedMovies) {
         this.formattedBooks = formattedMovies;
@@ -10,5 +12,11 @@ public class BooksPresenter {
     @Override
     public String toString() {
         return formattedBooks;
+    }
+
+    public void addBook(String name, String author, int year) {
+        formattedBooks = String.format("%-" + maximunNameLength + "s", name) + "|"
+                + String.format("%-" + maximumAuthorLength + "s", author) + "|"
+                + String.format("%-" + "4" + "s", year);
     }
 }
