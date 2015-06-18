bd6e3bfa1773db61ba2c9be30d3b4b11a13e35d5
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-18 10:23:48 +0530

    Added functionality to search a user in Users class

---
+++ Test/com/twu/biblioteca/UsersTest.java
@@ -0,0 +1,26 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class UsersTest {
+    @Test
+    public void shouldSearchTheAvailableUser() {
+        HashSet<User> listOfUsers=new HashSet<>();
+        ValidUser validUser = new ValidUser("9999-999", "priyar", "priya", "priya@gmail.com", 24714844);
+        listOfUsers.add(validUser);
+        listOfUsers.add(new ValidUser("9999-998","achu","aswini","achu@gmail.com",24714845));
+        listOfUsers.add(new ValidUser("9999-997","maddy","madhu","maddy@gmail.com",24714846));
+        Users users=new Users(listOfUsers);
+
+        User actualUser=users.login("9999-999","priyar");
+
+        assertEquals(validUser,actualUser);
+
+
+    }
+
+}
---
+++ src/com/twu/biblioteca/Users.java
@@ -0,0 +1,27 @@
+package com.twu.biblioteca;
+
+import java.util.HashSet;
+
+public class Users {
+    HashSet<User> users;
+
+    public Users(HashSet<User> users) {
+        this.users = users;
+    }
+
+    public User login(String userName, String password) {
+        return search(userName,password);
+
+    }
+
+    private User search(String UserId, String password) {
+        for (User user : users) {
+            if (user.MatchUser(UserId, password)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+}
