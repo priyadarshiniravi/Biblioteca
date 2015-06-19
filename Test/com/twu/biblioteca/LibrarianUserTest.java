154484db395401f2c8f52527ad68b1d66a7ad7fa
priyadarshiniravi <priyadharshiniravi03@gmail.com>
2015-06-19 10:53:34 +0530

    Refactored Librarian login message

--- Test/com/twu/biblioteca/LibrarianUserTest.java
+++ Test/com/twu/biblioteca/LibrarianUserTest.java
@@ -1,6 +1,5 @@
 package com.twu.biblioteca;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class LibrarianUserTest {
     @Test
     public void shouldReturnLoginMessage() {
-        ValidUser user = new ValidUser("3333-333", "password", "ishika", "emailid@gmail.com", 99857969);
+        LibrarianUser user = new LibrarianUser("3333-333", "password", "ishika", "emailid@gmail.com", 99857969);
 
         String actualLoginMessage = user.loginMessage();
 
